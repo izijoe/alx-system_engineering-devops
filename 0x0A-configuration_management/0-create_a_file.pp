c63c6fc9e0a8a27255a83429f72cab96a29625fc
izijoe <jospaco2001@yahoo.com>
2023-04-21 15:05:24 -0700

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file owened by www-data
+file { '/tmp/school':
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
