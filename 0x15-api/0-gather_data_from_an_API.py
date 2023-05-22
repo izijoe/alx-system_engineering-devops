cc0b1f0808ef6ec8632e153d0b51e3005ecf95ef
izijoe <jospaco2001@yahoo.com>
2023-05-22 13:49:48 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
-"""get data from jsonplaceholder"""
 import requests
 import sys
 
-if __name__ == '__main__':
-    """REST API manipulations"""
-    if len(sys.argv) > 1 and isinstance(eval(sys.argv[1]), int):
-        pass
-    else:
-        sys.exit(0)
 
-    BASE_API = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
-    user_response_url = BASE_API + "users/{}".format(employee_id)
-    todo_response_url = BASE_API + "users/{}/todos".format(employee_id)
+def tasks_done(id):
+    '''Script that displays an employee completed TODO tasks in stout
+        Parameters:
+        employee_id: Is an interger representing an employee id.
+    '''
 
-    user_response = requests.get(user_response_url).json()
-    todo_response = requests.get(todo_response_url).json()
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    response = requests.get(url)
+    response_json = response.json()
+    employee_name = response_json.get("name")
 
-    employee_name = user_response.get('name')
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    todos = requests.get(url)
+    todos_json = todos.json()
+    number_tasks = len(todos_json)
 
-    num_tasks_done = 0
-    for todo in todo_response:
-        if todo.get("completed"):
-            num_tasks_done += 1
+    task_compleated = 0
+    task_list = ""
 
-    total_num_tasks = 0
-    for todo in todo_response:
-        total_num_tasks += 1
+    for task in todos_json:
+        if task.get("completed") is True:
+            task_compleated += 1
+            task_list += "\t " + task.get("title") + "\n"
 
     print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          num_tasks_done, total_num_tasks))
+                                                          task_compleated,
+                                                          number_tasks))
+    print(task_list[:-1])
 
-    for todo in todo_response:
-        task_title = todo.get("title")
-        if (todo.get("completed")):
-            print("\t {}".format(task_title))
+
+if __name__ == "__main__":
+    tasks_done(sys.argv[1])
