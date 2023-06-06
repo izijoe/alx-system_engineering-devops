dbfbf4fdaed86c3d4f7e75a1a4134b0e0ed8da73
izijoe <jospaco2001@yahoo.com>
2023-06-06 15:27:26 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
-from requests import get
+"""Advanced Apis Module"""
+import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
+    """function that queries the Reddit API and returns the number
+    of subscribers (not active users, total subscribers) for a given
+    subreddit. If an invalid subreddit is given,
+    the function should return 0."""
 
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
+    headers = {"User-Agent": "user_agent"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(
+        url=url,
+        headers=headers,
+        allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()['data']
+        subscribers = data['subscribers']
+        return (subscribers)
+    else:
+        return (0)
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
 
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        subred = sys.argv[1]
+        number_of_subscribers(subred)
